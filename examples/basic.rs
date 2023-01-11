2f3aef5593e248e1afc096dfd874d94d87419cfb
Alexandria <alxpettit@gmail.com>
2023-01-11 16:03:35 -0700

    about ready to publish

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,8 +5,15 @@ fn main() {
     let mut args = Args::new();
     // Inserting a string type
     args.insert_string("greeting", String::from("hello world"));
+    // Inserting an i32 type
+    args.insert_i32("meaning_of_life", 42);
+    // There's a lot more types where that came from, BTW :)
+    // (In fact, you can use any type that implements `Any`, which... I think should be any?)
 
-    // Retrieving
+    // Retrieving string type
     let out = args.get_string("greeting").unwrap();
     println!("{}", out);
+    // Retrieving i32
+    let meaning_of_life = args.get_i32("meaning_of_life").unwrap();
+    println!("The meaning of life is: {}", meaning_of_life);
 }
