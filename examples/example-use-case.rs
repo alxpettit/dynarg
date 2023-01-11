82d6047c952ea45cb54289a6f3f3dede22faaadb
Alexandria <alxpettit@gmail.com>
2023-01-11 04:47:00 -0700

    sdoijsdojsdojsd

--- examples/example-use-case.rs
+++ examples/example-use-case.rs
@@ -1,32 +1,31 @@
-use dynarg;
+use dynarg::*;
 
-// /// Where normally you'd need to have a fixed set of arguments, each of which would be roughly fixed types
-// /// -- you can dynamically push arguments on the fly instead.
-// /// This is useful when you need a consistent function signature for different types of functions,
-// /// each needing different arguments
-// fn draw(args: &mut Args) {
-//     if let Some(arg) = args.get_string("thing_to_draw") {
-//         println!("I will draw {}!", arg);
-//         if let Some(size) = args.get_float("size") {
-//             println!("with a size of {}", size);
-//         }
-//     } else {
-//         panic!("Nothing to draw D:");
-//     }
-// }
-
+/// Where normally you'd need to have a fixed set of arguments, each of which would be roughly fixed types
+/// -- you can dynamically push arguments on the fly instead.
+/// This is useful when you need a consistent function signature for different types of functions,
+/// each needing different arguments
+fn draw(args: &mut Args) {
+    if let Some(arg) = args.get::<String>("thing_to_draw") {
+        println!("I will draw {}!", arg);
+        if let Some(size) = args.get::<f32>("size") {
+            println!("with a size of {}", size);
+        }
+    } else {
+        panic!("Nothing to draw D:");
+    }
+}
 
 fn main() {
-    // let mut args = Args::defaults();
-    //
-    // // This is how you add arguments
-    // args.insert("thing_to_draw", ArgData::String("apple".to_owned()));
-    // args.insert("size", ArgData::Float(5.2));
-    //
-    // draw(&mut args);
-    // if !args.all_used() {
-    //     println!("Warning! I didn't use all my arguments D:");
-    // }
-    // // Clear all the used flags on args
-    // args.reset_status();
+    let mut args = Args::default();
+
+    // This is how you add arguments
+    args.insert("thing_to_draw", Box::new("apple".to_owned()));
+    args.insert("size", Box::new(5.2));
+
+    draw(&mut args);
+    if !args.all_used() {
+        println!("Warning! I didn't use all my arguments D:");
+    }
+    // Clear all the used flags on args
+    args.reset_status();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,45 @@
+use indexmap::IndexMap;
+use num::Float;
+use num::Integer;
 use std::any::Any;
 use std::borrow::BorrowMut;
-use std::fmt::{Display, Formatter};
-use tracing::warn;
-use tracing::info;
-use indexmap::IndexMap;
 use std::fmt::Debug;
-use num::Integer;
-use num::Float;
+use std::fmt::{Display, Formatter};
+use tracing::info;
+use tracing::warn;
 
-struct ArgData(Box<dyn Any >);
-
+struct ArgData(Box<dyn Any>);
 
 struct Arg {
     data: ArgData,
-    used: bool
+    used: bool,
 }
 
 impl Arg {
     fn new(arg: Box<dyn Any>) -> Self {
         Self {
             data: ArgData(arg),
-            used: false
+            used: false,
         }
     }
 
     fn from_argdata(arg_data: ArgData) -> Self {
         Self {
             data: arg_data,
-            used: false
+            used: false,
         }
     }
 
-    fn get<T>(&mut self) -> Option<&T> where T: 'static {
+    fn get<T>(&mut self) -> Option<&T>
+    where
+        T: 'static,
+    {
         match self.data.0.downcast_ref::<T>() {
             Some(value) => {
                 self.used = true;
                 Some(value)
-            },
-            None => None
+            }
+            None => None,
         }
     }
 
@@ -46,7 +48,55 @@ impl Arg {
     }
 }
 
+#[derive(Default)]
+pub struct Args(IndexMap<String, Arg>);
 
+impl Args {
+    fn new() -> Self {
+        Self::default()
+    }
+    pub fn get<T>(&mut self, name: &str) -> Option<&T>
+    where
+        T: 'static,
+    {
+        match self.0.get_mut(name) {
+            None => None,
+            Some(value) => value.get::<T>(),
+        }
+    }
+    //
+    // fn get_string(&mut self, name: &str) -> Option<&str> {
+    //     // if let Some(v) = self.0.get::<String>(&name) {
+    //     //     Some(v)
+    //     // }
+    //     if let Some(v) = self.0.get::<String>(name) {
+    //         //println!("{:#?}", v);
+    //     }
+    //     None
+    // }
+
+    pub fn insert<S>(&mut self, name: S, value: Box<dyn Any>)
+    where
+        S: Into<String>,
+    {
+        self.0.insert(name.into(), Arg::new(value));
+    }
+
+    pub fn all_used(&self) -> bool {
+        for (_arg_name, arg) in &self.0 {
+            if arg.used {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    pub fn reset_status(&mut self) {
+        for (_arg_name, arg) in &mut self.0 {
+            arg.used = false;
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -58,17 +108,28 @@ mod tests {
         let mut arg = Arg::new(Box::new(a));
         assert_eq!(arg.get::<i32>(), Some(&5));
         assert_eq!(arg.used(), true);
+
+        let test = "apple";
+        let mut arg = Arg::new(Box::new(test));
+        assert_eq!(arg.get::<&str>(), Some(&"apple"));
+
+        let test2 = String::from("apple");
+        let mut arg2 = Arg::new(Box::new(test2));
+        assert_eq!(arg2.get::<String>(), Some(&"apple".to_string()));
+    }
+
+    #[test]
+    fn test_args() {
+        let mut args = Args::default();
+        args.insert("number".to_string(), Box::new(6));
+        let result = args.get::<i32>("number");
+        assert_eq!(result, Some(&6));
+
+        let static_str = "A";
+        args.insert("letter", Box::new(static_str));
+        // Note! This actually produces a &&str,
+        // as otherwise we would violate the Sized requirement on downcast_ref()
+        let arg = args.get::<&str>("letter");
+        assert_eq!(arg, Some(&"A"));
     }
 }
-
-//
-//
-// struct Args {
-//     data: IndexMap<Arg>
-// }
-//
-// impl Args {
-//     fn get_float<F>(&self) -> F where F: Float {
-//
-//     }
-// }
